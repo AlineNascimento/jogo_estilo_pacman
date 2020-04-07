c8002b0d8ea68a07ed4a502992c6db59308131ba
Aline Nascimento <alinephysics@gmail.com>
2020-04-07 20:36:35 -0300

    Movimentando o meu heroi

--- fogefoge.rb
+++ fogefoge.rb
@@ -7,10 +7,11 @@ def le_mapa(numero)
 end
 
 def encontra_jogador mapa
-    caractere_heroi = "H"
+    caractere_do_heroi = "H"
    mapa.each_with_index do |linha_atual, linha|
         coluna_do_heroi = linha_atual.index caractere_do_heroi
-        if coluna_do_heroi != nil
+        if coluna_do_heroi
+            return [linha,coluna_do_heroi]
         end
     end
 end
@@ -21,7 +22,23 @@ def joga(nome)
     while true
         desenha mapa
         direcao = pede_movimento
+        heroi = encontra_jogador mapa
+        mapa[heroi[0]][heroi[1]] = " "
+
+        case direcao
+        when "W"
+            heroi[0] -= 1
+        when "S"
+            heroi[0] += 1
+        when "D"
+            heroi[1] += 1
+        when "A"
+            heroi[1] -= 1
+        end
+        mapa[heroi[0]][heroi[1]] = "H"
     end
+    
+
     #jogo aqui
 end
 
