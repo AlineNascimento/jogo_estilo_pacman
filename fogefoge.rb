5010e8702d73ef06bcfbc1277e611f742b1cbf13
Aline Nascimento <alinephysics@gmail.com>
2020-04-08 18:03:17 -0300

    Ädicionando novo mapa ao decorrer do tempo

--- fogefoge.rb
+++ fogefoge.rb
@@ -53,56 +53,53 @@ def calcula_nova_posicao heroi, direcao
     heroi
 end
 
-def posicoes_validas_a_partir_de mapa, posicao
+def posicoes_validas_a_partir_de mapa, novo_mapa, posicao
     posicoes = []
     abaixo = [posicao[0] + 1,posicao[1]]
-    if posicao_valida? mapa, abaixo
+    
+    if (posicao_valida? mapa, abaixo) && (posicao_valida? novo_mapa, abaixo)
         posicoes << abaixo
     end
     acima = [posicao[0] - 1, posicao[1]]
-    if posicao_valida? mapa, acima
+    if (posicao_valida? mapa, acima) && (posicao_valida? novo_mapa, acima)
         posicoes << acima
     end
     direita = [posicao[0], posicao[1] + 1]
-    if posicao_valida? mapa, direita
+    if (posicao_valida? mapa, direita) && (posicao_valida? novo_mapa, direita)
         posicoes << direita
     end
     esquerda = [posicao[0], posicao[1] - 1]
-    if posicao_valida? mapa, esquerda
+    if (posicao_valida? mapa, esquerda) && (posicao_valida? novo_mapa, esquerda)
         posicoes << esquerda
     end
     posicoes
 end
 
-def move_fantasma mapa, linha, coluna
-    posicoes = posicoes_validas_a_partir_de mapa, [linha, coluna]
+def move_fantasma mapa, novo_mapa, linha, coluna
+    posicoes = posicoes_validas_a_partir_de mapa, novo_mapa, [linha, coluna]
     return if posicoes.empty?
     posicao = posicoes[0]
     mapa[linha][coluna] = " "
-    mapa[posicao[0]][posicao[1]] = "F"
+   novo_mapa[posicao[0]][posicao[1]] = "F"
 end
 
 def move_fantasmas mapa
     caractere_do_fantasma = "F" 
+    novo_mapa = copia_mapa mapa
     mapa.each_with_index do |linha_atual, linha|
         # .chars transforma um string num array de caracteres
         linha_atual.chars.each_with_index do |caractere_atual, coluna| 
         eh_fantasma = caractere_atual == caractere_do_fantasma
             if eh_fantasma
-                move_fantasma mapa, linha, coluna
+                move_fantasma mapa, novo_mapa, linha, coluna
             end
         end
     end
+    novo_mapa
 end
 
 def copia_mapa(mapa)
-   # mapa.join("\n")
-    novo_mapa = []
-    mapa.each do |linha|
-        nova_linha = nova_linha.dup.tr "F", " "
-        novo_mapa << nova_linha
-    end
-    novo_mapa
+   novo_mapa = mapa.join("\n").tr("F", " ").split "\n" 
 end
 
 def joga(nome)
@@ -120,7 +117,7 @@ def joga(nome)
 
         mapa[heroi[0]][heroi[1]] = "H"
 
-        move_fantasmas mapa
+       mapa = move_fantasmas mapa
     #jogo aqui
     end
 end
